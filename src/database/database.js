34dad7009232e799d358b2262781c4125e9e95f0
LuchoBre <lucho.bregoli@gmail.com>
2023-11-17 12:27:10 -0300

    se modifico dialectOptions

--- src/database/database.js
+++ src/database/database.js
@@ -4,5 +4,11 @@ const password = process.env.PASSWORDPS
 
 export const sequelize = new Sequelize('clinica', 'postgres', password, {
     host: 'localhost',
-    dialect:'postgres'
+    dialect:'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 });
