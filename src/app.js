afa01929c987d8770bda7c104a4b2cdcd772ca7c
LuchoBre <lucho.bregoli@gmail.com>
2023-11-17 15:11:42 -0300

    se cambio el cors

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://clinicalb.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
